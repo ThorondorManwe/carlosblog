7331ca44bba258c2d0adb6c07e7fe3b6bd63f425
Carlos Rangel <charlesleimaginant@gmail.com>
2022-05-01 12:16:21 -0500

    Add 404 page and styles

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/404.css'
+const Error = () => {
+    return (
+        <div id="notfound">
+            <div className="notfound">
+                <div className="notfound-404">
+                    <h3>Página no encontrada</h3>
+                    <h1><span>4</span><span>0</span><span>4</span></h1>
+                </div>
+                <h2>Lo siento, la página que buscas no existe</h2>
+            </div>
+        </div>
+    )
+}
+export default Error;
