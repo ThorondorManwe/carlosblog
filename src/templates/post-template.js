e7b3eac95b3ffbf67f4e8f84b9d410e346c124b0
Carlos Rangel <charlesleimaginant@gmail.com>
2022-05-05 02:22:34 -0500

    Add author details to blogposts with social media accounts using react-icons

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -4,26 +4,35 @@ import { Link, graphql } from "gatsby"
 import Image from "gatsby-image"
 import Layout from "../components/Layout"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { FaGithub, FaTwitterSquare, FaDev, FaLinkedin } from "react-icons/fa"
 
 const postTemplate = ( {data}) => {
-  const { title, date, author, image } = data.mdx.frontmatter;
+  const { title, date, author, twitter, github, dev, linkedin, image } = data.mdx.frontmatter;
   const { body } = data.mdx;
   const img = image.childImageSharp.fluid;
   return (
     <Layout>
       <section className={styles.template}>
        <Link to="/" className={styles.link}>
-         back to all posts
+         regresar al inicio
        </Link>
          <div className={styles.info}>
            <h1>{title}</h1>
            <h4>
-             <span>by {author}</span> / <span>{date}</span>
+             <span>por {author}</span> / <span>{date}</span>
            </h4>
          </div>
          <Image fluid={img} />
          <div className={styles.content}>
            <MDXRenderer>{body}</MDXRenderer>
+         </div>
+         <div className={styles.author}>
+           <h1>{author}</h1>
+           {twitter && <a href={twitter} target="_blank" rel="noopener noreferrer">
+                          {<FaTwitterSquare />}</a>}
+           {github && <a href={github} target="_blank" rel="noopener noreferrer">{<FaGithub />}</a>}
+           {dev && <a href={dev} target="_blank" rel="noopener noreferrer">{<FaDev />}</a>}
+           {linkedin && <a href={linkedin} target="_blank" rel="noopener noreferrer">{<FaLinkedin />}</a>}
          </div>
       </section>
     </Layout>
@@ -37,6 +46,10 @@ query getPost($slug: String!) {
             slug
             date(formatString: "MMMM Do, YYYY")
             author
+            twitter
+            github
+            dev
+            linkedin
             image {
             childImageSharp {
                     fluid {
